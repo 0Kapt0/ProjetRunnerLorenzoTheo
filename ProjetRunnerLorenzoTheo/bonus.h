1c0b4b7525f9685a029083242f5d91be5d91af10
Toupetit2 <theo.doublet@yahoo.com>
2025-10-23 12:05:33 +0200

    Create bonus class

--- ProjetRunnerLorenzoTheo/bonus.h
+++ ProjetRunnerLorenzoTheo/bonus.h
@@ -1,5 +1,13 @@
 #pragma once
-class bonus
+#include "SFML/Graphics.hpp"
+class Bonus
 {
+protected:
+	sf::Vector2f position;
+
+public:
+	sf::Vector2f getPosition();
+
+	virtual void draw(sf::RenderWindow& window);
 };
 
